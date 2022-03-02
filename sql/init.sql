d6abbbfafedb168b83c7aac910085ef31c513eef
Ethan Hirsch <ethan@hirschfamily.com>
2022-03-02 12:45:18 -0800

    fixed caps issues

--- sql/init.sql
+++ sql/init.sql
@@ -87,95 +87,95 @@ VALUES
     ('Study Chicken', 'Try to understand why chicken crossed the road', 2, '2013-01-08', 4, True, 3, 1, 3);
 
 -- get all projectID, projectName, and projectDesc to populate Project’s Page --
-SELECT * FROM projects;
+SELECT * FROM Projects;
 
 -- get all memberID, memberFName, memberLName, and memberEmail to populate Member’s page –
-SELECT * FROM members;
+SELECT * FROM Members;
 
 -- get all teamID, teamName, teamDesc, and projectName to populate team’s page  --
-SELECT teams.teamID, teams.teamName, teams.teamDesc, projects.projectName
-FROM teams
-INNER JOIN projects ON teams.teamID=projects.projectID;
+SELECT Teams.teamID, Teams.teamName, Teams.teamDesc, Projects.projectName
+FROM Teams
+INNER JOIN Projects ON Teams.teamID=Projects.projectID;
 
 --  get all projectName to populate team’s column “Project” dropdown with all project names where teamProjectID=projectID –
-SELECT projectName FROM projects;
+SELECT projectName FROM Projects;
 
 -- get all mapID, memberID, teamID to populate membersteam’s page  --
-SELECT *  from membersteams;
+SELECT *  from MembersTeams;
 OR
 --  get all mapID, memberEmail, teamName to populate membersteam’s page --
-SELECT mapID, members.memberEmail, teams.teamName
-FROM membersteams
-INNER JOIN members ON membersteams.memberID=members.memberID
-INNER JOIN teams on membersteams.teamID=teams.teamID;
+SELECT mapID, Members.memberEmail, Teams.teamName
+FROM MembersTeams
+INNER JOIN Members ON MembersTeams.memberID=Members.memberID
+INNER JOIN Teams on MembersTeams.teamID=Teams.teamID;
 
 --  get all taskID, taskName, taskDesc, taskPriority, taskDeadline, taskDifficulty, taskDone, taskProjectID, taskTeamID, taskMemberID to populate task’s page –
-SELECT * FROM tasks;
+SELECT * FROM Tasks;
 OR
 --  get all taskID, taskName, taskDesc, taskPriority, taskDeadline, taskDifficulty, taskDone, projectName, teamName, memberEmail to populate task’s page --
-SELECT tasks.taskID, tasks.taskName, tasks.taskDesc, tasks.taskPriority, tasks.taskDeadline, tasks.taskDifficulty, tasks.taskDone, projects.projectName, teams.teamName, members.memberEmail
-FROM tasks
-INNER JOIN projects ON tasks.taskProjectID=projects.projectID
-INNER JOIN teams on tasks.taskTeamID=teams.teamID
-INNER JOIN members on tasks.taskMemberID=members.memberID;
+SELECT Tasks.taskID, Tasks.taskName, Tasks.taskDesc, Tasks.taskPriority, Tasks.taskDeadline, Tasks.taskDifficulty, Tasks.taskDone, Projects.projectName, Teams.teamName, Members.memberEmail
+FROM Tasks
+INNER JOIN Projects ON Tasks.taskProjectID=Projects.projectID
+INNER JOIN Teams on Tasks.taskTeamID=Teams.teamID
+INNER JOIN Members on Tasks.taskMemberID=Members.memberID;
 
 -- get single task for the search form –
-SELECT * FROM tasks WHERE taskName = {{taskName}};
+SELECT * FROM Tasks WHERE taskName = {{taskName}};
 
 -- get single project for the search form –
-SELECT * FROM projects WHERE projectName = {{projectName}};
+SELECT * FROM Projects WHERE projectName = {{projectName}};
 
 -- get single member for the search form –
-SELECT * FROM members WHERE memberEmail = {{memberEmail}};
+SELECT * FROM Members WHERE memberEmail = {{memberEmail}};
 
 -- add new project –
-INSERT INTO projects (projectName, projectDesc) VALUES 
+INSERT INTO Projects (projectName, projectDesc) VALUES 
 ({{projectName}}, {{projectDesc}});
 
 -- add new member –
-INSERT INTO members (memberFName, memberLName, memberEmail) VALUES 
+INSERT INTO Members (memberFName, memberLName, memberEmail) VALUES 
 ({{memberFName}}, {{memberLName}}, {{memberEmail}});
 
 -- add new team –
-INSERT INTO teams (teamName, teamDesc, teamProjectID)
+INSERT INTO Teams (teamName, teamDesc, teamProjectID)
 VALUES 
     ({{teamName}}, {{teamDesc}}, {{teamProjectID}});
 
 -- add new members_teams –
-INSERT INTO membersteams (memberID, teamID)
+INSERT INTO MembersTeams (memberID, teamID)
 VALUES 
     ({{memberID}}, {{teamID}});
 
 -- add new task –
-INSERT INTO tasks (taskName, taskDesc, taskPriority, taskDeadline, taskDifficulty, taskDone, taskProjectID, taskTeamID, taskMemberID)
+INSERT INTO Tasks (taskName, taskDesc, taskPriority, taskDeadline, taskDifficulty, taskDone, taskProjectID, taskTeamID, taskMemberID)
 VALUES 
-({{taskName}}, {{taskDesc}}, {{taskPriority}}, {{taskDeadline}}, {{taskDifficulty}}, {{taskDone}}, {{taskProjectID}}, {{taskTeamID}}, {{teaskMemberID}})
+({{taskName}}, {{taskDesc}}, {{taskPriority}}, {{taskDeadline}}, {{taskDifficulty}}, {{taskDone}}, {{taskProjectID}}, {{taskTeamID}}, {{taskMemberID}})
 
 -- update project data based on user submission of form --
-UPDATE projects SET projectName={{projectName}}, projectDesc={{projectDesc}} WHERE projectID={{projectID}};
+UPDATE Projects SET projectName={{projectName}}, projectDesc={{projectDesc}} WHERE projectID={{projectID}};
 
 -- update member data based on user submission of form -- 
-UPDATE members SET memberFName={{memberFName}}, memberLName={{memberLName}}, memberEmail={{memberEmail}} WHERE memberID={{memberID}};
+UPDATE Members SET memberFName={{memberFName}}, memberLName={{memberLName}}, memberEmail={{memberEmail}} WHERE memberID={{memberID}};
 
 -- update team data based on user submission of form --
-UPDATE teams SET teamName={{teamName}}, teamDesc={{teamDesc}}, teamProjectID={{teamProjectID}} WHERE teamID={{teamID}};
-- update membersteams based on user submission of form --
-UPDATE membersteams SET memberID={{memberID}}, teamID={{teamID}} WHERE mapID={{mapID}};
+UPDATE Teams SET teamName={{teamName}}, teamDesc={{teamDesc}}, teamProjectID={{teamProjectID}} WHERE teamID={{teamID}};
+-- update membersteams based on user submission of form --
+UPDATE MembersTeams SET memberID={{memberID}}, teamID={{teamID}} WHERE mapID={{mapID}};
 
 -- update task based on user submission of form –
-UPDATE tasks SET taskName={{taskName}}, taskDesc={{taskDesc}}, taskPriority={{taskPriority}}, taskDeadline={{taskDeadline}}, taskDifficulty={{taskDifficulty}}, taskDone={{taskDone}}, taskProjectID={{taskProjectID}}, taskTeamID={{taskTeamID}}, taskMemberID={{taskMemberID}} WHERE taskID={{taskID}};
+UPDATE Tasks SET taskName={{taskName}}, taskDesc={{taskDesc}}, taskPriority={{taskPriority}}, taskDeadline={{taskDeadline}}, taskDifficulty={{taskDifficulty}}, taskDone={{taskDone}}, taskProjectID={{taskProjectID}}, taskTeamID={{taskTeamID}}, taskMemberID={{taskMemberID}} WHERE taskID={{taskID}};
 
 -- delete project data based on user submission of form –
-DELETE FROM projects WHERE projectID={{projectID}};
+DELETE FROM Projects WHERE projectID={{projectID}};
 
 -- delete member data based on user submission of form –
-DELETE FROM members WHERE memberID={{memberID}};
+DELETE FROM Members WHERE memberID={{memberID}};
 
 -- delete team data based on user submission of form –
-DELETE FROM teams WHERE teamID={{teamID}}
+DELETE FROM Teams WHERE teamID={{teamID}}
 
 -- delete membersteams data based on user submission of form –
-DELETE FROM membersteams WHERE mapID={{mapID}};
+DELETE FROM MembersTeams WHERE mapID={{mapID}};
 
 -- delete task data based on user submission of form –
-DELETE FROM tasks WHERE taskID={{taskID}}
+DELETE FROM Tasks WHERE taskID={{taskID}}
