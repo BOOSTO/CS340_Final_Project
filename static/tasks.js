2b494805fa7533cd0d6156ffb1bcd396531f9eb7
Ethan Hirsch <ethan@hirschfamily.com>
2022-03-11 18:10:10 -0800

    Tasks UI is complete

--- static/tasks.js
+++ static/tasks.js
@@ -13,7 +13,7 @@ taskProjectInput.addEventListener('change', function (event) {
     }
     // add the default option again
     const option = document.createElement("option");
-    option.value = "NULL";
+    option.value = "";
     option.innerText = "Select Team";
     option.selected = true;
     option.disabled = true;
@@ -49,7 +49,7 @@ taskTeamInput.addEventListener('change', function (event) {
     }
     // add the default option again
     const option = document.createElement("option");
-    option.value = "NULL";
+    option.value = "";
     option.innerText = "Select Member";
     option.selected = true;
     option.disabled = true;
@@ -82,7 +82,10 @@ team_selects = document.getElementsByClassName("select-team");
 member_selects = document.getElementsByClassName("select-member");
 for (let i = 0; i < id_inputs.length; ++i) {
     // isolate a single row
-    task_id = id_inputs[i].value;
+    const task_id = id_inputs[i].value;
+    const project_select = project_selects[i]
+    const team_select = team_selects[i]
+    const member_select = member_selects[i]
 
     //populate its teams options
     {
@@ -97,11 +100,11 @@ for (let i = 0; i < id_inputs.length; ++i) {
                     const option = document.createElement("option");
                     option.value = team.teamID;
                     option.innerText = team.teamName;
-                    team_selects[i].appendChild(option);
+                    team_select.appendChild(option);
                     if (first_element) {
-                        team_selects[i].value = option.value;
+                        team_select.value = option.value;
                         if (option.value == "") {
-                            member_selects[i].disabled = true;
+                            member_select.disabled = true;
                         }
                         first_element = false;
                     }
@@ -125,9 +128,9 @@ for (let i = 0; i < id_inputs.length; ++i) {
                     const option = document.createElement("option");
                     option.value = member.memberID;
                     option.innerText = member.fullName;
-                    member_selects[i].appendChild(option);
+                    member_select.appendChild(option);
                     if (first_element) {
-                        member_selects[i].value = option.value;
+                        member_select.value = option.value;
                         first_element = false;
                     }
                 });
@@ -135,6 +138,93 @@ for (let i = 0; i < id_inputs.length; ++i) {
         };
         req.open("GET", "members-by-task?task_id="+task_id, true);
         req.send();
+    }
+
+    // set up event listeners on options
+    {
+        // update the task's teams list when a new task project is selected
+        project_select.addEventListener('change', function (event) {
+            team_select.disabled = false;
+            member_select.disabled = true;
+
+            // remove all children of taskTeamInput
+            while(team_select.childElementCount > 0) {
+                team_select.removeChild(team_select.lastChild);
+            }
+            // add the default option again
+            const option = document.createElement("option");
+            option.value = "";
+            option.innerText = "No Team";
+            option.selected = true;
+            team_select.appendChild(option);
+
+            {
+                 // remove all children of taskMemberInput
+                while(member_select.childElementCount > 0) {
+                    member_select.removeChild(member_select.lastChild);
+                }
+                // add the default option again
+                const option = document.createElement("option");
+                option.value = "";
+                option.innerText = "No Assignee";
+                option.selected = true;
+                member_select.appendChild(option);
+            }
+
+            // populate with result of a new query
+            var selectedTaskProject = project_select.value;
+            const req = new XMLHttpRequest();
+            req.onload = function() {
+                var res = req.response;
+                if (req.status == 200) {
+                    // add each team to list
+                    teams = JSON.parse(req.responseText)
+                    teams.forEach(team => {
+                        const option = document.createElement("option");
+                        option.value = team.teamID;
+                        option.innerText = team.teamName;
+                        team_select.appendChild(option);
+                    });
+                } else console.log("ERROR: couldn't get teams list");
+            };
+            req.open("GET", "teams-by-proj?project_id="+selectedTaskProject, true);
+            req.send();
+        });
+
+        // update the task's members list when a new task team is selected
+        team_select.addEventListener('change', function (event) {
+            member_select.disabled = false;
+
+            // remove all children of taskMemberInput
+            while(member_select.childElementCount > 0) {
+                member_select.removeChild(member_select.lastChild);
+            }
+            // add the default option again
+            const option = document.createElement("option");
+            option.value = "";
+            option.innerText = "No Assignee";
+            option.selected = true;
+            member_select.appendChild(option);
+
+            // populate with result of a new query
+            var selectedTaskTeam = team_select.value;
+            const req = new XMLHttpRequest();
+            req.onload = function() {
+                var res = req.response;
+                if (req.status == 200) {
+                    // add each team to list
+                    members = JSON.parse(req.responseText);
+                    members.forEach(member => {
+                        const option = document.createElement("option");
+                        option.value = member.memberID;
+                        option.innerText = member.fullName;
+                        member_select.appendChild(option);
+                    });
+                } else console.log("ERROR: couldn't get members list");
+            };
+            req.open("GET", "members-by-team?team_id="+selectedTaskTeam, true);
+            req.send();
+        });
     }
 }
 
